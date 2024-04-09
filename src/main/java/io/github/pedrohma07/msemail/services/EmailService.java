b56174ee3f4720dac3f7901e3e88be09c3d11ed7
pedrohma07 <pedrohma07@gmail.com>
2024-04-09 11:06:43 -0300

    Add email sending functionality to EmailService

--- src/main/java/io/github/pedrohma07/msemail/services/EmailService.java
+++ src/main/java/io/github/pedrohma07/msemail/services/EmailService.java
@@ -1,17 +1,47 @@
 package io.github.pedrohma07.msemail.services;
 
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import io.github.pedrohma07.msemail.enums.StatusEmail;
 import io.github.pedrohma07.msemail.models.EmailModel;
 import io.github.pedrohma07.msemail.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class EmailService {
 
     @Autowired
+    private AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
     private EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
-
+    public EmailModel sendEmail(EmailModel emailModel) {
+        try {
+            SendEmailRequest request = new SendEmailRequest()
+                    .withDestination(
+                            new Destination().withToAddresses(emailModel.getEmailTo()))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withText(new Content()
+                                            .withCharset("UTF-8").withData(emailModel.getText())))
+                            .withSubject(new Content()
+                                    .withCharset("UTF-8").withData(emailModel.getSubject())))
+                    .withSource(emailModel.getEmailFrom());
+            amazonSimpleEmailService.sendEmail(request);
+            emailModel.setStatusEmail(StatusEmail.SENT);
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailRepository.save(emailModel);
+        } catch (Exception ex) {
+            System.out.println("O e-mail não foi enviado.");
+            System.out.println("Detalhes do erro: " + ex.getMessage());
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+            emailRepository.save(emailModel);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
     }
 }
